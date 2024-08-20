acfd3122ada7946dc21fe3c3a5a8fc10bf72fa74
skylanwei <city_invoker@qq.com>
2024-08-20 14:57:07 +0800

    update solar term

--- util/solar_term_calender.h
+++ util/solar_term_calender.h
@@ -3,13 +3,61 @@
 #ifndef TRPC_COMMON_UTIL_SOLAR_TERM_CALENDER_H_
 #define TRPC_COMMON_UTIL_SOLAR_TERM_CALENDER_H_
 
+#include "trpc/util/singleton.h"
+
 namespace trpc {
 namespace app {
 
 
 static const std::vector<std::string> solar_tg = {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"}; 
 static const std::vector<std::string> solar_dz = {"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"}; 
-static const uint64_t anchor_point = 0; //the timestamp of gz first point
+
+static const std::vector<std::string> solar_terms = {"立春","雨水","惊蛰","春分","清明","谷雨","立夏","小满","芒种","夏至","小暑","大暑","立秋","处暑","白露","秋分","寒露","霜降","立冬","小雪","大雪","冬至",  "小寒","大寒"};
+
+//a tropical year is 365 days 5 hours 48 minutes 46 seconds, equal to 31556926 seconds,
+//we use 31556928(two more seconds) to make it divisible by 12 to define the month's
+//len is 2629744 seconds
+//static const uint32_t tropical_year_len = 31556928;
+using tropical_years = std::chrono::duration<int64_t, std::ratio<31556928>>;
+using tropical_months = std::chrono::duration<int64_t, std::ratio<2629744>>;
+using tropical_days = std::chrono::duration<int64_t, std::ratio<24 * 60 * 60>>;
+using tropical_hours = std::chrono::duration<int64_t, std::ratio<60 * 60 * 2>>; //一个时辰等于2h
+
+std::chrono::system_clock::time_point DateStrToTimePoint(std::string& datetime) {
+  std::tm tm = {};
+  std::istringstream ss(datetime);
+  ss >> std::get_time(&tm, "%Y-%m-%d %H:%M:%S");
+  st_tm.tm_isdst = -1;
+  std::time_t tt = std::mktime(&st_tm);
+  std::chrono::system_clock::time_point tp = std::chrono::system_clock::from_time_t(tt);
+  return tp;
+}
+
+class AnchorPoint : public Singleton<AnchorPoint, CreateUsingNew, DefaultLifetime> {
+
+private:
+  AnchorPoint() {
+    //甲子年丙寅月甲寅日立春起始点
+    std::string year_month_anchor_str = "1924-02-05 09:49:00";
+    year_month = DateStrToTimePoint(year_month_anchor_str);
+
+    //甲子年丙寅月甲寅日甲子时起始点
+    std::string day_hour_anchor_str = "1924-02-04 23:00:00";
+    day_hour = DateStrToTimePoint(day_hour_anchor_str);
+  }
+
+public:
+  std::chrono::system_clock::time_point YearMonth() {
+    return year_month;
+  }
+  std::chrono::system_clock::time_point DayHour() {
+    return day_hour;
+  }
+
+private:
+  std::chrono::system_clock::time_point year_month;
+  std::chrono::system_clock::time_point day_hour;
+};
 
 class GZ {
 public:
@@ -26,25 +74,36 @@ private:
 
 class SolarTerm {
 public:
-  SolarTerm(uint64_t ts) : ts(ts) {
-    date = trpc::TimeStringHelper::ConvertUnixTimeToStr(ts);
-    uint64_t diff_ts = ts - anchor_point;
-    uint32_t y_idx = std::ceil(diff_ts / 31556952); //365.2425 days, a solar circle seconds
-    uint32_t m_idx = std::ceil(diff_ts / 2629746);  //30.436875 days 1/12 of a year
-    uint32_t d_idx = std::ceil(diff_ts / 86400);
-    uint32_t h_idx = std::ceil(diff_ts / 3600 * 2);
-    gz_year = GZ(y_idx % 10, y_idx % 12);
-    gz_month = GZ(m_idx % 10, m_idx % 12);
-    gz_day = GZ(d_idx % 10, d_idx % 12);
-    gz_hour = GZ(h_idx % 10, h_idx % 12);
-  }
 
-  SolarTerm(const std::string& datetime) : SolarTerm(trpc::time::ConvertHttpDateToEpoch(datetime)) {
+  SolarTerm(const std::string& datetime) : datetime(datetime) {
+    tp = DateStrToTimePoint(datetime);
+
+    //this algorithm is wrong for solar term's year and months, coz the tropical point of 
+    //the sun can not be caculate. but we can define the tropical year's duration, and divid it 
+    //into 12 months to make every solar term is close to the real tropical time,
+    //diviations within 10 mins.
+    auto ym_dur = tp - AnchorPoint::GetInstance()->YearMonth();
+    uint32_t elapse_years = std::chrono::duration_cast<tropical_years>(ym_dur).count();
+    uint32_t elapse_months = std::chrono::duration_cast<tropical_months>(ym_dur).count();
+
+    gz_year = GZ(y_idx % 10, y_idx % 12); //for anchor point's offset idx is g=0,z=0
+    gz_month = GZ(m_idx % 10 + 2, m_idx % 12 + 2); //for anchor point's offset idx is g=2,z=2
+
+    //for day and hour, this algorithm is absolutely right,
+    //coz there's no relation between tropical day and month,
+    //for the day's gz is tick 1 for every 24 hours
+    //and for the hours's gz is tick 1 for every 2 hours
+    auto dh_dur = tp - AnchorPoint::GetInstance()->DayHour();
+    uint32_t elapse_days = std::chrono::duration_cast<tropical_days>(dh_dur).count();
+    uint32_t elapse_hours = std::chrono::duration_cast<tropical_hours>(dh_dur).count();
+    gz_day = GZ(elapse_days % 10, elapse_days % 12 + 2); //for anchor point's offset idx is g=0,z=2
+    gz_hour = GZ(elapse_hours % 10, elapse_hours % 12); //for anchor point's offset idx is g=0,z=0
+
   }
 
 private:
-  uint64_t ts;
-  std::string date;
+  std::string datetime;
+  std::chrono::system_clock::time_point tp;
   GZ gz_year;
   GZ gz_month;
   GZ gz_day;

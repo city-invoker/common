72e5341b26bf80c72b0161e5d0271359442b5600
skylanwei <city_invoker@qq.com>
2024-08-23 15:06:42 +0800

    debug

--- util/solar_term_calender.h
+++ util/solar_term_calender.h
@@ -9,8 +9,9 @@ namespace trpc {
 namespace app {
 
 
-static const std::vector<std::string> solar_tg = {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"}; 
-static const std::vector<std::string> solar_dz = {"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"}; 
+static const uint32_t kGzLen = 60;
+static const std::vector<std::string> kTg = {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"}; 
+static const std::vector<std::string> kDz = {"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"}; 
 
 static const std::vector<std::string> solar_terms = {"立春","雨水","惊蛰","春分","清明","谷雨","立夏","小满","芒种","夏至","小暑","大暑","立秋","处暑","白露","秋分","寒露","霜降","立冬","小雪","大雪","冬至",  "小寒","大寒"};
 
@@ -61,17 +62,35 @@ private:
 class GZ {
 public:
   GZ() {}
-  GZ(uint32_t idx_g, uint32_t idx_z) : idx_g(idx_g), idx_z(idx_z) {
-    name = solar_tg[idx_g] + solar_dz[idx_z];
+  GZ(uint32_t idx) : idx(idx) {
+    name.append(kTg[idx % kTg.size()]);
+    name.append(kDz[idx % kDz.size()]);
+  }
+
+  uint32_t GetIdx() {
+    return idx;
+  }
+
+  uint32_t GetIdxG() {
+    return idx % kTg.size();
+  }
+
+  uint32_t GetIdxrZ() {
+    return idx % kDz.size();
   }
 
   std::string GetName() {
     return name;
   }
 
+  std::string dump() {
+    std::stringstream ss;
+    ss << idx << ":" << name;
+    return ss.str();
+  }
+
 private:
-  uint32_t idx_g; //index of tg in range 0-9
-  uint32_t idx_z; //index of dz in range 0-11 
+  uint32_t idx;   //index of tgdz in range 0-59
   std::string name; //gz in chinese
 };
 
@@ -79,18 +98,19 @@ class SolarTerm {
 public:
 
   SolarTerm(const std::string& datetime) : datetime(datetime) {
-    tp = DateStrToTimePoint(datetime);
 
     //this algorithm is wrong for solar term's year and months, coz the tropical point of 
     //the sun can not be caculate. but we can define the tropical year's duration, and divid it 
     //into 12 months to make every solar term is close to the real tropical time,
     //diviations within 10 mins.
+
+    std::chrono::system_clock::time_point tp = DateStrToTimePoint(datetime);
     auto ym_dur = tp - AnchorPoint::GetInstance()->YearMonth();
     uint32_t elapse_years = std::chrono::duration_cast<tropical_years>(ym_dur).count();
     uint32_t elapse_months = std::chrono::duration_cast<tropical_months>(ym_dur).count();
 
-    gz_year = GZ(elapse_years % 10, elapse_years % 12); //for anchor point's offset idx is g=0,z=0
-    gz_month = GZ(elapse_months % 10 + 2, elapse_months % 12 + 2); //for anchor point's offset idx is g=2,z=2
+    gz_year = GZ(elapse_years % kGzLen);
+    gz_month = GZ((elapse_months + 2) % kGzLen); //丙寅's offset is 2
 
     //for day and hour, this algorithm is absolutely right,
     //coz there's no relation between tropical day and month,
@@ -99,40 +119,66 @@ public:
     auto dh_dur = tp - AnchorPoint::GetInstance()->DayHour();
     uint32_t elapse_days = std::chrono::duration_cast<tropical_days>(dh_dur).count();
     uint32_t elapse_hours = std::chrono::duration_cast<tropical_hours>(dh_dur).count();
-    gz_day = GZ(elapse_days % 10, elapse_days % 12 + 2); //for anchor point's offset idx is g=0,z=2
-    gz_hour = GZ(elapse_hours % 10, elapse_hours % 12); //for anchor point's offset idx is g=0,z=0
+    gz_day = GZ((elapse_days + 51) % kGzLen); //甲寅's offset is 51
+    gz_hour = GZ(elapse_hours % kGzLen);
   }
 
   std::string GetDateTime() {
     return datetime;
   }
 
-  GZ GZYear() {
+  GZ GetYear() {
     return gz_year;
   }
 
-  GZ GZMonth() {
+  GZ GetMonth() {
     return gz_month;
   }
 
-  GZ GZDay() {
+  GZ GetDay() {
     return gz_day;
   }
 
-  GZ GZHour() {
+  GZ GetHour() {
     return gz_hour;
   }
 
+  std::vector<uint32_t> GetIdxs() {
+    std::vector<uint32_t> v(4);
+    v[0] = gz_year.GetIdx();
+    v[1] = gz_month.GetIdx();
+    v[2] = gz_day.GetIdx();
+    v[3] = gz_hour.GetIdx();
+    return v;
+  }
+
+  std::vector<std::string> GetNames() {
+    std::vector<std::string> v(4);
+    v[0] = gz_year.GetName();
+    v[1] = gz_month.GetName();
+    v[2] = gz_day.GetName();
+    v[3] = gz_hour.GetName();
+    return v;
+  }
+
+  std::string dump() {
+    std::stringstream ss;
+    ss << datetime << ":["
+       << gz_year.dump() << ","
+       << gz_month.dump() << ","
+       << gz_day.dump() << ","
+       << gz_hour.dump() << "]";
+    return ss.str();
+  }
 
 private:
   std::string datetime;
-  std::chrono::system_clock::time_point tp;
   GZ gz_year;
   GZ gz_month;
   GZ gz_day;
   GZ gz_hour;
-};
 
+};
 
 }
 }
